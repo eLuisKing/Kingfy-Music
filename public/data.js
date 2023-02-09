80757e59fddafd02ce4ed53834513759716ac86c
LuisSantillan96 <luistecisc@gmail.com>
2023-02-08 19:28:59 -0600

    Base data

---
+++ public/data.js
@@ -0,0 +1,8 @@
+const data = [
+    {
+        'song': 'moscow mule',
+        'disc': 'un verano sin ti',
+        'anho': 2022,
+        'link': 'https://www.youtube.com/watch?v=p38WgakuYDo&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=5'
+    }
+]
