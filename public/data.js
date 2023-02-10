f7d3f9ec3b4559920b472c91ba363e80f440052b
LuisSantillan96 <luistecisc@gmail.com>
2023-02-09 20:14:33 -0600

    updated data

--- public/data.js
+++ public/data.js
@@ -2,7 +2,139 @@ const data = [
     {
         'song': 'moscow mule',
         'disc': 'un verano sin ti',
-        'anho': 2022,
+        'year': 2022,
         'link': 'https://www.youtube.com/watch?v=p38WgakuYDo&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=5'
-    }
+    },
+    {
+        'song': 'despues de la playa',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=a5rlgE6dcBY&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=6'
+    },
+    {
+        'song': 'me porto bonito',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=saGYMhApaH8&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=3'
+    },
+    {
+        'song': 'titi me pregunto',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=Cr8K88UcO0s&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=4'
+    },
+    {
+        'song': 'un ratito',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=tGhyQ7KanMU&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=9'
+    },
+    {
+        'song': 'yo no soy celoso',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=kLUeP9E9f5k&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=10'
+    },
+    {
+        'song': 'tarot',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=gwJ7YKhmWKg&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=11'
+    },
+    {
+        'song': 'neverita',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=htJMGFKOs8c&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=12'
+    },
+    {
+        'song': 'la corriente',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=A95yW-LVnbY&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=13'
+    },
+    {
+        'song': 'efecto',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=Nk8C9FdCdJQ&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=14'
+    },
+    {
+        'song': 'party',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=G9XrXnfJPsk&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=15'
+    },
+    {
+        'song': 'aguacero',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=iBYndEtOVv4&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=16'
+    },
+    {
+        'song': 'ensenhame a bailar',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=eopPKANmKyg&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=17'
+    },
+    {
+        'song': 'ojitos lindos',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=10EX-_h4pYc&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=18'
+    },
+    {
+        'song': 'dos mil 16',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=czx0dQe-3uk&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=19'
+    },
+    {
+        'song': 'el apagon',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=QdQEljUMCEM&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=20'
+    },
+    {
+        'song': 'otro atardecer',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=VBiNZcH27Y8&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=21'
+    },
+    {
+        'song': 'un coco',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=lH3d69sbvX4&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=22'
+    },
+    {
+        'song': 'andrea',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=gjvTQTGogUM&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=23'
+    },
+    {
+        'song': 'me fui de vacaciones',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=7K7Sl0FPsVo&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=24'
+    },
+    {
+        'song': 'un verano sin ti',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=REyv4cblksI&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=25'
+    },
+    {
+        'song': 'agosto',
+        'disc': 'un verano sin ti',
+        'year': 2022,
+        'link': 'https://www.youtube.com/watch?v=QYQSzoWx_28&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=26'
+    },
+    {
+        'song': 'callaita',
+        'disc': 'un verano sin ti',
+        'year': 2019,
+        'link': 'https://www.youtube.com/watch?v=acEOASYioGY&list=PLRW7iEDD9RDStpKHAckdbGs3xaCChAL7Z&index=27'
+    },
 ]
